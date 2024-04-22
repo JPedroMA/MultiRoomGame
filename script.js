11979a429cb7f9b2a8012ad35503cc5fdd5de075
João Pedro <aspuma13@gmail.com>
2024-04-21 21:13:39 -0300

    Starting of plataform game
    trying out new things

---
+++ script.js
@@ -0,0 +1,42 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 64 * 16; //1044
+canvas.height = 64 * 9; //576
+
+const backgroundLevel1 = new Sprite({
+    position :{
+        x: 0,
+        y: 0,
+    },
+    imageSrc: './img/backgroundLevel1.png',
+})
+
+const player = new Player();
+
+const keys = {
+    w: {
+        pressed: false,
+    },
+    a: {
+        pressed: false,
+    },
+    d: {
+        pressed: false,
+    },
+}
+function animate() {
+    window.requestAnimationFrame(animate);
+
+    backgroundLevel1.draw();
+
+    player.velocity.x = 0;
+    if (keys.d.pressed) player.velocity.x = 5;
+    else if (keys.a.pressed) player.velocity.x = -5;
+    
+    player.draw();
+    player.update();
+}
+
+animate();
+
